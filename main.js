0c027f11a16b4bd37575288bb67b05e024fd7b72
YoDevil <alessandro.astone@hotmail.com>
2017-05-23 17:38:40 +0200

    Added margin between input images

--- main.js
+++ main.js
@@ -264,6 +264,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
         var image = document.createElement("img");
         image.setAttribute("src",input_canvas.toDataURL("image/png"));
         image.setAttribute("value",input_number.value);
+        image.style.margin=2;
         image.addEventListener("click", function(){
             this.remove();
         });
